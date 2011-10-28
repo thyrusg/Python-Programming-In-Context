b746a5768558a1d48a8638c7f6182d96b6c7a040
Thyrus <thyrusg@thyrusgorges.com>
2011-10-28 09:57:26 -0400

    Question #3 added to Lab 16
    Signed-off-by: Thyrus <thyrusg@thyrusgorges.com>

--- ClassLabs/Lab16/Lab16.py
+++ ClassLabs/Lab16/Lab16.py
@@ -3,27 +3,41 @@
 
 def myReadlines(openfile, number):
     listoflines = []
+    cc = 0
     for item in range(0,number):
-        listoflines.append(f.readline())
-        listoflines = listoflines[:number]
-    if len(listoflines) > number:
-        for item in listoflines:
-            if item == "":
-                listoflines.remove(item)
-            elif item != "":
-                pass
-        
+       listoflines.append(openfile.readline())
+    #listoflines = listoflines[:number]
+    for item in listoflines:
+       if item == "":
+            cc = cc + 1
+       elif item != "":
+            pass
+    lenoflist = len(listoflines)
+    if cc > 1:
+        listoflines = listoflines[:(lenoflist-cc)]
+    else:
+        pass
     return listoflines
-    
-######################################33
-#for item in range(0,number):
-#...     lister.append(f.readline())
 
-#The above statement adds all items except the first to the list
-
-#def myReadlines(openfile, number):
-#...     listoflines = []
-#...     for item in range(0,number):
-#...         listoflines.append(f.readline())
-#...         listoflines = listoflines[:number]
-#...     return listoflines
+# 3: Write a function that opens a web page & returns a dictionary of all the links
+# and their text on that page. A link is defined by an HTML tag. The link is
+# everything in quotes after the "<a href=" and before ">", and the text is 
+# everything between the > and </a>. Use the following algorithm to process the page
+# 
+# Open the web page.
+# While there remains some characters in line:
+# 	i. if line contains <a href=
+# 		a. slice line so that everyting up to <a href= is removed
+#		b. find >
+#		c. url = text from beginning of line to the character before >
+# 		d. print url
+#		e. slice line so that > is removed
+# 		f. find </a>
+#		g. text = text from beginning of line to the character before </a>
+#		h. print text
+# 		i. add {url:text}
+#	ii. else truncate line
+#
+# The resulting dictionary is to be written to an output txt file, where each dict
+# element (pair) is written on a new line. You can assume that all html tags are all
+# in lower case. Download the test file 'Test.html' to test the code.
