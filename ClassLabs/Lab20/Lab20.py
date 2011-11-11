221fef8db120baeb40f6540aac6bb2d40b2d3e63
Thyrus <thyrusg@thyrusgorges.com>
2011-11-11 10:08:11 -0500

    Added Lab 20
    Signed-off-by: Thyrus <thyrusg@thyrusgorges.com>

---
+++ ClassLabs/Lab20/Lab20.py
@@ -0,0 +1,15 @@
+# Write a recursive function to draw concentric nested boxes where each box is centered
+# around the same point. It might be helpful to write a helper function called
+# drawCenteredSquare that accepts parameters for a turtle, the center x and y positions,
+# and the length of the side.
+
+
+# Write the tree function where you can add color to the tree by making the large
+# branches brown, and the small branches green. Choose a threshold value for the length
+# of the the trunk and set the color accordingly.
+
+# You have learned the Sierpinski Triangle from the book. Modify the sierpinski and 
+# drawTriangle function to add color. [Hint: Make each recursive call draw a different
+# color.]
+
+
