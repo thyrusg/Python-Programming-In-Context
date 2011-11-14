fb05123cf49fe5d40d3d83ce6c2aa0ef528214c3
Thyrus <thyrusg@thyrusgorges.com>
2011-11-13 20:03:40 -0500

    Removed project1.py and Added pro.py
    Signed-off-by: Thyrus <thyrusg@thyrusgorges.com>

---
+++ Projects/Project1/pro.py
@@ -0,0 +1,59 @@
+# Name: Thyrus Gorges
+# Access ID: eo8112
+
+
+def earthquakeStats(file):
+    f = open(file, 'r')
+
+    removeHeader = f.readline()  # Removes the header of the file
+    postHeader = f.tell()
+
+    name = []  # List of all the region names
+    greatMag = []  # Magnitudes of 8+
+    majorMag = []  # Magnitudes between 7 - 7.9
+    strongMag = []  # Magnitudes between 6 - 6.9
+    moderateMag = []  # Magnitudes between 5 - 5.9
+
+    for line in f:
+        split = line.split('\t')
+        place = split[6]
+        if place not in name:
+            name.append(place)
+        else:
+            pass
+
+    for i in name:
+        greatMag.append(0)
+        majorMag.append(0)
+        strongMag.append(0)
+        moderateMag.append(0)
+
+    f.seek(postHeader)  # Go to the spot after reading the header
+
+    for line in f:
+        split = line.split('\t')
+        mag = float(split[1])
+        place = split[6]
+        placeIndex = name.index(place)
+        if mag >= 8:
+            greatMag[placeIndex] = greatMag[placeIndex] + 1
+        elif mag >= 7 and mag <= 7.9:
+            majorMag[placeIndex] = majorMag[placeIndex] + 1
+        elif mag >= 6 and mag <= 6.9:
+            strongMag[placeIndex] = strongMag[placeIndex] + 1
+        elif mag >= 5 and mag <= 5.9:
+            moderateMag[placeIndex] = moderateMag[placeIndex] + 1
+        else:
+            pass
+    filename = input('Please include the .csv extension')
+    outputname = open(filename, 'w')
+    print('Please enter the name of the file where the out will be stored')
+    outputname.write("%s %s %s %s %s %s" % ('REGION,', 'MODERATE,', 'STRONG',
+    'MAJOR,', 'GREAT,', 'OVERALL,'))
+
+    outputname.write("%s %i %i %i %i %i" % (name[i], moderateMag[i],
+    strongMag[i], majorMag[i], greatMag[i]))
+
+
+
+    #return greatMag, name, majorMag, strongMag, moderateMag
