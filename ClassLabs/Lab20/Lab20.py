2959edf02502fc02ea7fbed699d94861c9d50198
Thyrus <thyrusg@thyrusgorges.com>
2011-11-15 13:28:05 -0500

    Added Lab 20
    Signed-off-by: Thyrus <thyrusg@thyrusgorges.com>

--- ClassLabs/Lab20/Lab20.py
+++ ClassLabs/Lab20/Lab20.py
@@ -1,15 +1,28 @@
-# Write a recursive function to draw concentric nested boxes where each box is centered
-# around the same point. It might be helpful to write a helper function called
-# drawCenteredSquare that accepts parameters for a turtle, the center x and y positions,
-# and the length of the side.
+import cTurtle
+
+# Write a recursive function to draw concentric nested boxes where each box is
+# centered around the same point. It might be helpful to write a helper
+# function'called drawCenteredSquare that accepts parameters for a turtle, the
+# center x and y positions, and the length of the side.
+
+def drawSquare(t,p1,p2,p3,p4):
+    t.up()
+    t.goto(p1)
+    t.down()
+    t.goto(p2)
+    t.goto(p3)
+    t.goto(p4)
+
+def midPoint(p1,p2):
+    return ((p1[0]+p2[0]) / 2.0, (p1[1]+p2[1] / 2.0))
 
 
-# Write the tree function where you can add color to the tree by making the large
-# branches brown, and the small branches green. Choose a threshold value for the length
-# of the the trunk and set the color accordingly.
+# Write the tree function where you can add color to the tree by making the
+# large branches brown, and the small branches green. Choose a threshold value
+# for the length of the the trunk and set the color accordingly.
 
-# You have learned the Sierpinski Triangle from the book. Modify the sierpinski and 
-# drawTriangle function to add color. [Hint: Make each recursive call draw a different
-# color.]
+# You have learned the Sierpinski Triangle from the book. Modify the sierpinski
+# and drawTriangle function to add color. [Hint: Make each recursive call draw
+# a different color.]
 
 
