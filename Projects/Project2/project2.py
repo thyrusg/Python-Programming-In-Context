9e391721560e7dfca8cae708da3dc3ac148946c7
Thyrus <thyrusg@thyrusgorges.com>
2011-11-22 00:30:20 -0500

    Started Project # 2
    Signed-off-by: Thyrus <thyrusg@thyrusgorges.com>

---
+++ Projects/Project2/project2.py
@@ -0,0 +1,36 @@
+class Phone:
+	def __init__(self, manufactuer, model, price, quantity):
+		self.manufact = manufactuer
+		self.model = model
+		self.price = price
+		self.quantity = quantity
+
+	def set_manufact(self, newManfact):
+		self.manufact = newManfact
+
+	def set_model(self, newModel):
+		self.model = newModel
+
+	def set_retail_price(self, newPrice):
+		self.price = newPrice
+
+	def set_quantity(self, newQuantity):
+		self.quantity = newQuantity
+
+	def get_manufact(self):
+		return self.manufact
+
+	def get_model(self):
+		return self.model
+
+	def get_retail_price(self):
+		return self.price
+
+	def get_quantity(self):
+		return self.quantity
+
+	# To-do: 
+	# Create a Store class. 
+	# Store should have a list of objects of type Phone
+	# An attribute that represents the number of phones.
+	# Create a initializer, add_phone, and remove_phone.
