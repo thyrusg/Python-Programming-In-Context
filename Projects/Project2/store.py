84dde89cf96f557474d3922a80dfa0becbd4d3b0
Thyrus <thyrusg@thyrusgorges.com>
2011-12-10 14:18:02 -0500

    Fixed bugs in store.py
    Signed-off-by: Thyrus <thyrusg@thyrusgorges.com>

--- Projects/Project2/store.py
+++ Projects/Project2/store.py
@@ -4,29 +4,45 @@ class Store:
         self.phones = []
         self.num_of_phones = 0
 
-    def update_quantity(self, amount):
+    def add_quantity(self, amount):
         """Update the amount of phones in the store"""
         self.num_of_phones += amount
 
+    def remove_quantity(self, amount):
+        """Remove amount from number of phones"""
+        self.num_of_phones -= amount
+
     def add_phone(self):
         """Add phones to the store. Add a way to check if phone is in store"""
-        newPhone = []
+        newPhone = []  # List containing the phones data
         manufacturer = input("Enter the name of the manufacturer: ")
-        newPhone.append(manufacturer)  # Index is 0
         model = input("Enter the model: ")
-        newPhone.append(model)  # Index is 1
-        for item in range(0, len(self.phones)):
-            if manufacturer and model in self.phones[item]:
-                print("That phone is already in the store")
-                break
-            else:
-                pass
-        price = int(input("Enter the price: "))
-        newPhone.append(price)  # Index is 2
+        price = float(input("Enter the price: "))
         quantity = int(input("Enter the quantity: "))
-        newPhone.append(quantity)  # Index is 3
-        self.phones.append(newPhone)
-        self.update_quantity(quantity)
+        if len(self.phones) == 0:
+            newPhone.append(manufacturer)
+            newPhone.append(model)
+            newPhone.append(price)
+            newPhone.append(quantity)
+            self.phones.append(newPhone)
+            self.add_quantity(quantity)
+        elif len(self.phones) > 0:
+            for item in range(0, len(self.phones)):
+                if manufacturer and model in self.phones[item]:
+                    print("That phone is already in the store")
+                    print("The extra quantity was added to the phone")
+                    self.phones[item][3] += quantity
+                    self.add_quantity(quantity)
+                    break
+                elif manufacturer and model not in self.phones[item]:
+                    newPhone.append(manufacturer)
+                    newPhone.append(model)
+                    newPhone.append(price)
+                    newPhone.append(quantity)
+                    self.phones.append(newPhone)
+                    self.add_quantity(quantity)
+                else:
+                    pass
 
     def remove_phone(self):
         """Remove phone from the store"""
@@ -39,8 +55,11 @@ class Store:
                     print("%s %s %s %s" % (self.phones[item][0],
                     self.phones[item][1], self.phones[item][2],
                     self.phones[item][3]))
+                    self.remove_quantity(self.phones[item][3])
                     self.phones.remove(self.phones[item])
                     print("The phone was succesfully removed")
+                    #self.remove_quantity(self.phones[item][3])
+                    break
                 else:
                     print("The phone was not removed")
             else:
@@ -48,6 +67,7 @@ class Store:
 
     def get_number_of_phones(self):
         """Return the amount of phones in the store"""
+        print(self.num_of_phones)
         return self.num_of_phones
 
     def update_phone_info(self):
@@ -58,9 +78,9 @@ class Store:
         for item in range(0, len(self.phones)):
             if manufacturer and model in self.phones[item]:
                 print("What would you like to update: ?")
-                answer = int(input("[1] Update manufacturer \n",
-                "[2] Update Model \n,"
-                "[3] Update Price \n", "[4] Update Quantity \n", "[5] Exit"))
+                answer = int(input("[1] Update manufacturer \n"
+                "[2] Update Model \n"
+                "[3] Update Price \n" "[4] Update Quantity \n" "[5] Exit \n"))
                 if answer == 1:
                     newManu = input("Enter the new manufacturer: ")
                     self.phones[item][0] = newManu
@@ -72,6 +92,13 @@ class Store:
                     self.phones[item][2] = newPrice
                 elif answer == 4:
                     newQuantity = int(input("Enter the new quantity: "))
+                    originalQuantity = self.phones[item][3]
+                    if originalQuantity < newQuantity:
+                        self.add_quantity(newQuantity - originalQuantity)
+                    elif originalQuantity > newQuantity:
+                        self.remove_quantity(originalQuantity - newQuantity)
                     self.phones[item][3] = newQuantity
                 elif answer == 5:
                     break
+            else:
+                pass
